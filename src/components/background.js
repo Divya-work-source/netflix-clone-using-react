c300889d67ee98cbd4627b307f0be5f72f58d495
divya/ <divya@bereej.com>
2025-09-29 17:47:23 +0530

    q

--- src/components/background.js
+++ src/components/background.js
@@ -1,6 +1,6 @@
 // src/components/background.js
 import React from 'react';
-import './background.css'; // Optional: if you have styles
+
 
 export default function Background({ children, ...restProps }) {
   return <div className="background" {...restProps}>{children}</div>;
